af3892fe13708fd0213f182ed512f02c372116a0
miccu64 <98konrad98@gmail.com>
2023-06-26 23:24:24 +0200

    Added db init & docker compose

--- AuctionApp_SSR/database/database.js
+++ AuctionApp_SSR/database/database.js
@@ -1,7 +1,8 @@
 import { Sequelize } from 'sequelize'
 
-export const sequelize = new Sequelize('AuctionApp', 'sa', 'Agilero123!', {
+export const sequelize = new Sequelize('AuctionApp', 'sa', 'Haslo123!', {
   dialect: 'mssql',
+  host: 'database',
   dialectOptions: {
     options: {
       useUTC: false,
