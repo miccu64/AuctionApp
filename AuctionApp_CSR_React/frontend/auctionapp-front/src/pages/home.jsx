e7c5a3521c9f2dd547f83fcbafa2cf3ed285d34a
Konrad Micek <98konrad98@gmail.com>
2023-07-01 16:06:41 +0200

    Added navbar

---
+++ AuctionApp_CSR_React/frontend/auctionapp-front/src/pages/home.jsx
@@ -0,0 +1,20 @@
+import { Typography } from '@mui/material'
+
+function Home() {
+  return (
+    <main>
+      <div />
+      <div>
+        <Typography variant="h6">Title</Typography>
+      </div>
+      <div>
+        <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc maximus, nulla ut commodo sagittis, sapien dui
+          mattis dui, non pulvinar lorem felis nec erat
+        </Typography>
+      </div>
+    </main>
+  )
+}
+
+export default Home
